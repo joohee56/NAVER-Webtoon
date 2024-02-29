8b104465faaa85bbca7a06ed3fd41c2868d51d83
Joohui <joohuii96@gmail.com>
2024-02-29 23:46:51 +0900

    [BE] feat: project setting

---
+++ backend/naver-webtoon/src/main/java/jh/naverwebtoon/NaverWebtoonApplication.java
@@ -0,0 +1,13 @@
+package jh.naverwebtoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaverWebtoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaverWebtoonApplication.class, args);
+	}
+
+}
---
+++ backend/naver-webtoon/src/test/java/jh/naverwebtoon/NaverWebtoonApplicationTests.java
@@ -0,0 +1,13 @@
+package jh.naverwebtoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaverWebtoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
