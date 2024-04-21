bbaac970f3e1de306c7aca9890f46b5da672d9f3
Joohui <joohuii96@gmail.com>
2024-04-21 19:11:18 +0900

    [BE] feat: 새 웹툰 생성 시 입력값 검증

--- backend/naver-webtoon/src/main/java/jh/naverwebtoon/dto/request/CreateWebtoonReq.java
+++ backend/naver-webtoon/src/main/java/jh/naverwebtoon/dto/request/CreateWebtoonReq.java
@@ -1,6 +1,9 @@
 package jh.naverwebtoon.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.util.List;
 import jh.naverwebtoon.db.domain.enums.GenreEnum;
 import jh.naverwebtoon.db.domain.enums.WebtoonCategory;
@@ -16,12 +19,18 @@ import org.springframework.web.multipart.MultipartFile;
 @NoArgsConstructor
 public class CreateWebtoonReq {
     @NotBlank
+    @Size(min = 1, max = 10)
     private String name;
+    @NotNull
     private WebtoonCategory webtoonCategory;
+    @NotEmpty
     private List<GenreEnum> genres;
     private List<String> tags;
     @NotBlank
+    @Size(min = 1, max = 10)
     private String oneLineSummary;
+    @NotBlank
+    @Size(min = 1, max = 400)
     private String summary;
     private MultipartFile posterImage;
     private MultipartFile horizontalImage;
